8048b31acfdbcc1ae45aaab6d694610c2449a1cf
Roxana Radu <roxana_radu95@yahoo.com>
2015-06-24 12:52:52 +0300

    Improved coding style

--- get_elevi.py
+++ get_elevi.py
@@ -17,27 +17,24 @@ def main():
                 print >> sys.stderr, "Argument is not a valid filename"
                 usage()
                 sys.exit(1)
-        mingrade=10
-        maxgrade=0        
-        
+        mingrade = 10
+        maxgrade = 0
         for line in list(fp):
-                #print "linia curenta este %s" %(line)
-                line=line.strip()
-                items=line.split('|')
-                grade=int(items[3])
-                if grade<mingrade:
-                        mingrade=grade
-                        prenumemin=items[0]
-                        numemin=items[1]
-                        grupamin=items[2]
-                if grade>maxgrade:
-                        maxgrade=grade
-                        prenumemax=items[0]
-                        numemax=items[1]
-                        grupamax=items[2]
-        print "Nota maxima este %d iar info student:  %s %s %s" %(maxgrade,prenumemax,numemax,grupamax)
-        print "Nota miima este %d iar info student: %s %s %s " %(mingrade,prenumemin,numemin,grupamin)
+                line = line.strip()
+                items = line.split('|')
+                grade = int(items[3])
+                if grade < mingrade:
+                        mingr = grade
+                        minpren = items[0]
+                        minnume = items[1]
+                        mingr = items[2]
+                if grade > maxgrade:
+                        maxgrade = grade
+                        prenumemax = items[0]
+                        numemax = items[1]
+                        grupamax = items[2]
+        print "MAX: %d, info student:  %s %s %s" % (maxgrade, prenumemax, numemax, grupamax)
+        print "MIN: %d, info student: %s %s %s " % (mingr, minpren, minnume, mingr)
 
 if __name__ == "__main__":
         sys.exit(main())
-
